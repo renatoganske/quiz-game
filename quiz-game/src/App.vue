e4496c014a1dea8227e4c25fa7be7fe854e14d0a
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-06-08 22:55:04 -0300

    feat: apresentando as opcoes de resposta

--- quiz-game/src/App.vue
+++ quiz-game/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <h1 v-html="this.question"></h1>
 
-    <input type="radio" name="options" value="True">
-    <label>Verdadeiro</label><br>
+    <template v-if="this.question">
 
-    <input type="radio" name="options" value="False">
-    <label>Falso</label><br>
+      <h1 v-html="this.question">
+      </h1>
+
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input type="radio" name="options" value="answer">
+        <label v-html="answer"></label><br>
+      </template>
+
+    </template>
 
     <button class="send" type="button">Enviar</button>
   </div>
